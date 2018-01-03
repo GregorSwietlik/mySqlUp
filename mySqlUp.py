9493a8b3e796f0bdc640f1928212018e006657c9
greg <gregor@swietlik.fr>
2018-01-03 16:33:18 +0100

    connect change.

--- mySqlUp.py
+++ mySqlUp.py
@@ -15,11 +15,11 @@ db01_select = ("SELECT a1, b1, c1 FROM db.table WHERE a1 = 'xxxx'")
 db02_truncate = ("TRUNCATE db.table")
 db02_insert = ("INSERT INTO db.table (a2, b2, c2) VALUES (%s, %s, %s)")
 
-import mysql.cnxector 
+import mysql.connector 
 
-cnx_db01 = mysql.cnxector.cnxect(host=db01_host, user=db01_user, password=db01_password)
+cnx_db01 = mysql.connector.connect(host=db01_host, user=db01_user, password=db01_password)
 db01 = cnx_db01.cursor()
-cnx_db02 = mysql.cnxector.cnxect(host=db02_host, user=db02_user, password=db02_password)
+cnx_db02 = mysql.connector.connect(host=db02_host, user=db02_user, password=db02_password)
 db02 = cnx_db02.cursor()
 db02.execute(db02_truncate)
 db01.execute(db01_select)
